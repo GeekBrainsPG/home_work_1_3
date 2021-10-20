0c01e9d07e6e2d18ffe9169aed0b9b18259ea364
Petr GUlchuk <petr.gulchuk@gmail.com>
2021-10-20 17:09:22 -0400

    created flipBinaryArray method for the first task

--- src/main/java/HomeWorkLauncher.java
+++ src/main/java/HomeWorkLauncher.java
@@ -1,7 +1,17 @@
+import service.HomeWorkService;
+
+import java.util.Arrays;
+
 public class HomeWorkLauncher {
 
     public static void main(String[] args) {
+        // 1 task
+        byte[] bytes = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        byte[] result = HomeWorkService.flipBinaryArray(bytes);
 
+        System.out.println(Arrays.toString(result));
+
+        //2 task
     }
 
 }
